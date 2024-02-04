15c6677e777a6eaec2a2da9a67ab4c9f8994150b
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2024-02-04 08:11:38 +0530

    usecontext hook done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,32 @@
 import React, { useContext } from "react";
+import "./cart-module.css";
 import { CartContext } from "../context/CartContext";
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
   return (
-    <div>
+    <div className="cart">
       <h1>your cart items will come here</h1>
-      {cart.length === 0 && <h1>pls add items to the cart</h1>}
-      {cart.map((data) => {
-        return (
-          <div key={data.id}>
-            <img src={data.thumbnail} alt="" />
-            <h5>{data.title}</h5>
-            <h3>price {data.price}</h3>
-          </div>
-        );
-      })}
-      <h1>Total {cart.reduce((total, data) => (total += data.price), 0)}</h1>;
+      <div>
+        {cart.length === 0 && <h1>pls add items to the cart</h1>}
+        {cart.map((data) => {
+          return (
+            <div className="cart-layout" key={data.id}>
+              <div>
+                <img src={data.thumbnail} alt="" />
+              </div>
+              <div>
+                <h2>{data.brand}</h2>
+                <h5>{data.title}</h5>
+
+                <h3>price {data.price}</h3>
+                <p>{data.description}</p>
+              </div>
+            </div>
+          );
+        })}
+        <h1>Total {cart.reduce((total, data) => (total += data.price), 0)}</h1>
+      </div>
     </div>
   );
 };
